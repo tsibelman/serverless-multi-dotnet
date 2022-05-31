dbaee570f4031c2a9ac8098cdb010e7a7de66c52
Dor Danai <ddanai@csod.com>
2022-05-31 11:45:05 +0300

    adding support to dotnet6 runtime and arm64 arc

--- index.js
+++ index.js
@@ -9,17 +9,22 @@ class ServerlessDotNet {
   constructor(serverless, options) {
     this.serverless = serverless;
     this.options = options;
-
-    if(!options["nopack"]){
-      
-      Object.assign( this, pack );
-      Object.assign( this, getPackingInfo );
-      Object.assign( this, funcRuntimeIsDotNet )
-
+    // console.log(options);
+    // console.log(getPackingInfo);
+    // console.log(serverless);
+    // console.log(options);
+    // options[""]
+    // getPackingInfo.newOptions = options;
+    // pack.newOptions = options;
+    console.log(options)    
+    if (!options["nopack"]) {      
+      Object.assign(this, pack);
+      Object.assign(this, getPackingInfo);
+      Object.assign(this, funcRuntimeIsDotNet);            
       this.hooks = {
         'package:createDeploymentArtifacts': () => BbPromise.bind(this).then(this.pack)
       };
-    } 
+    }
   }
 }
 
